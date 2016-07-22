c37352a8b9d9ac137f645ee98709d407e1399bf3
kateryna <katrusyabb@gmail.com>
2016-07-22 17:07:37 +0300

    Update content

--- js/script.js
+++ js/script.js
@@ -1,16 +1,46 @@
 var app = angular.module("myApp", []);
+
+app.directive("creatEvent", function() {
+    return {
+        restrict : "E",
+        templateUrl : 'creating.html'
+    };
+});
+
+app.directive("editEvent", function() {
+    return {
+        restrict : "E",
+        templateUrl : 'editing.html'
+    };
+});
+ 
 app.controller("MyController", ['$scope', '$http', function($scope, $http) {
 
   $scope.saved = localStorage.getItem('todos');
-  $scope.todos = (localStorage.getItem('todos') !== null) ? JSON.parse($scope.saved) : [{
+  $scope.todos = (localStorage.getItem('todos') !== null) ? JSON.parse($scope.saved) : [
+  {
     text: 'lorem',
-    info: 'About Lorem',
     details: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam maxime blanditiis placeat exercitationem, tenetur minus ex sed officiis magni, pariatur officia ratione eaque voluptas.',
     done: false
-  }, {
+  },
+  {
     text: 'lorem1',
-    info: '1About Lorem',
-    details: '1Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam maxime blanditiis placeat exercitationem, tenetur minus ex sed officiis magni, pariatur officia ratione eaque voluptas. ',
+    details: '1Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam maxime blanditiis placeat exercitationem, tenetur minus ex sed officiis magni, pariatur officia ratione eaque voluptas.',
+    done: false
+  },
+  {
+    text: 'lorem2',
+    details: '2Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam maxime blanditiis placeat exercitationem, tenetur minus ex sed officiis magni, pariatur officia ratione eaque voluptas.',
+    done: false
+  },
+  {
+    text: 'lorem3',
+    details: '3Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam maxime blanditiis placeat exercitationem, tenetur minus ex sed officiis magni, pariatur officia ratione eaque voluptas.',
+    done: false
+  },
+  {
+    text: 'lorem4',
+    details: '4Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam maxime blanditiis placeat exercitationem, tenetur minus ex sed officiis magni, pariatur officia ratione eaque voluptas. ',
     done: false
   }];
 
@@ -25,17 +55,15 @@ app.controller("MyController", ['$scope', '$http', function($scope, $http) {
 
   $scope.editableTodo = {};
 
-  //Add
+  //Add new Event
   $scope.addItem = function() {
-    if ($scope.todoText && $scope.todoInfo) {
+    if ($scope.todoText && $scope.todoDetails) {
       $scope.todos.push({
         text: $scope.todoText,
-        info: $scope.todoInfo,
         details: $scope.todoDetails,
         done: false
       });
       $scope.todoText = '';
-      $scope.todoInfo = '';
       $scope.todoDetails = '';
 
       localStorage.setItem('todos', JSON.stringify($scope.todos));
@@ -44,24 +72,20 @@ app.controller("MyController", ['$scope', '$http', function($scope, $http) {
   };
   $scope.cancelAdd = function() {
     $scope.todoText = '';
-    $scope.todoInfo = '';
+    $scope.todoDetails = '';
   };
+  
   //Edit
   $scope.editItem = function(x) {
     $scope.isEditing = true;
-    $scope.isEditing1 = false;
-    /*$scope.todoText =$scope.todos[x].text;
-		$scope.todoInfo = $scope.todos[x].info;*/
     Object.assign($scope.editableTodo, $scope.todos[x], {
       index: x
     });
   };
 
   $scope.updateItem = function(x) {
-    if ($scope.editableTodo.text && $scope.editableTodo.info) {
-      /*	$scope.todos[x].text = $scope.todoText;
-			$scope.todos[x].info = $scope.todoInfo;
-        */
+    if ($scope.editableTodo.text && $scope.editableTodo.details) {
+      
       var index = $scope.editableTodo.index;
       delete $scope.editableTodo.index;
 
@@ -72,34 +96,19 @@ app.controller("MyController", ['$scope', '$http', function($scope, $http) {
     }
     localStorage.setItem('todos', JSON.stringify($scope.todos));
   };
-  // Remaining
-  $scope.remaining = function() {
-    var count = 0;
-    angular.forEach($scope.todos, function(todo) {
-      count += todo.done ? 1 : 0;
-    });
-    return count;
-  };
-  //Remove Multiple Item
-  $scope.removeMulItem = function() {
-    var oldTodos = $scope.todos;
-    $scope.todos = [];
-    angular.forEach(oldTodos, function(todo) {
-      if (!todo.done)
-        $scope.todos.push(todo);
-    });
-    localStorage.setItem('todos', JSON.stringify($scope.todos));
-  };
-  // REmove Single Item
+
+  // Remove Single Item
   $scope.removeSingleItem = function(x) {
     $scope.todos.splice(x, 1);
     localStorage.setItem('todos', JSON.stringify($scope.todos));
   };
   // Create Item
   $scope.isCreating = false;
+  $scope.isEditing = false;
 
   $scope.startCreating = function() {
     $scope.isCreating = true;
+    $scope.isEditing = false;
   };
 
   $scope.cancelCreating = function() {
@@ -108,13 +117,11 @@ app.controller("MyController", ['$scope', '$http', function($scope, $http) {
   };
   // Edit Item
   $scope.startEditing = function() {
-    // $scope.isCreating = false;
+    $scope.isCreating = false;
     $scope.isEditing = true;
-    $scope.isEditing1 = true; //
   };
   $scope.cancelEditing = function() {
     $scope.isEditing = false;
-    $scope.isEditing1 = false; //
     $scope.editableTodo = {};
   };
   //next/prev page
@@ -132,22 +139,23 @@ app.controller("MyController", ['$scope', '$http', function($scope, $http) {
     }
     var next = $scope.currentPage;
     $scope.selectedItem = $scope.todos[next];
-
   };
 
-$scope.editorEnabled = false;
+$scope.editorEnabled = true;
   
   $scope.enableEditor = function() {
-    $scope.editorEnabled = true;
+    $scope.editorEnabled = false;
     $scope.editableDetails = $scope.selectedItem.details;
+    $scope.editableText = $scope.selectedItem.text;
   };
   
   $scope.disableEditor = function() {
-    $scope.editorEnabled = false;
+    $scope.editorEnabled = true;
   };
   
   $scope.save = function() {
     $scope.selectedItem.details = $scope.editableDetails;
+    $scope.selectedItem.text = $scope.editableText;
     $scope.disableEditor();
     localStorage.setItem('todos', JSON.stringify($scope.todos));
   };
